c0c06d796f30dc4a80efa98a3b5eb15b6119fa3b
akorn22 <akorn22@a-korn.dev>
2025-11-20 16:27:48 +0100

    feat: error-less mapgen?

--- unnamed/src/GameMap/MapGeneration/GraphBasedGenerator.cs
+++ unnamed/src/GameMap/MapGeneration/GraphBasedGenerator.cs
@@ -6,38 +6,40 @@ public class GraphBasedGenerator : IMapGenerator
 {
     private const int ChunkSizeSquared = Map.ChunkSize * Map.ChunkSize;
     private readonly Random rng = Random.Shared;
-    private readonly int roomMaxSize = 10;
-    private readonly int roomMinSize = 5;
+    private readonly int roomMaxSize = 12;
+    private readonly int roomMinSize = 6;
 
-    private readonly float roomsPerChunk = 3;
+    private readonly float roomTriesPerChunk = 3;
 
-    public void GenerateMap(in TileFlags[,] map)
+    public void GenerateMap(in IntermediateMap map)
     {
-        int width = map.GetLength(0);
-        int height = map.GetLength(1);
+        int width = map.Width;
+        int height = map.Height;
 
-        int roomCount = (int)(this.roomsPerChunk * width * height / ChunkSizeSquared);
+        int roomTries = (int)(this.roomTriesPerChunk * width * height / ChunkSizeSquared);
 
         List<Rect> rooms = new();
         List<(int x, int y)> centers = new();
 
-        for (int i = 0; i < roomCount; i += 1)
+        for (int i = 0; i < roomTries; i += 1)
         {
             int w = this.rng.Next(this.roomMinSize, this.roomMaxSize + 1);
             int h = this.rng.Next(this.roomMinSize, this.roomMaxSize + 1);
-            int x = this.rng.Next(1, width - w - 1);
-            int y = this.rng.Next(1, height - h - 1);
+            int x = this.rng.Next(1, width - w);
+            int y = this.rng.Next(1, height - h);
 
             Rect newRoom = new(x, y, w, h);
             bool overlaps = false;
+
             foreach (Rect r in rooms)
             {
-                if (!newRoom.Intersects(r))
-                {
-                    continue;
-                }
+                Rect inflated = r.Inflate(2);
 
-                overlaps = true;
+                if (inflated.Intersects(newRoom))
+                {
+                    overlaps = true;
+                    break;
+                }
             }
 
             if (overlaps)
@@ -47,6 +49,7 @@ public class GraphBasedGenerator : IMapGenerator
 
             rooms.Add(newRoom);
             centers.Add(newRoom.Center);
+            // Console.WriteLine($"Room: x {newRoom.X1}, y {newRoom.Y1}, x2 {newRoom.X2}, y2 {newRoom.Y2}, center {newRoom.Center}");
             CarveRoom(map, newRoom);
         }
 
@@ -55,9 +58,11 @@ public class GraphBasedGenerator : IMapGenerator
         {
             this.CarveCorridor(map, centers[i - 1], centers[i]);
         }
+
+        while (EraseIllegalWalls(in map)) { }
     }
 
-    private static void CarveRoom(in TileFlags[,] map, Rect r)
+    private static void CarveRoom(in IntermediateMap map, Rect r)
     {
         for (int x = r.X1; x <= r.X2; x += 1)
         for (int y = r.Y1; y <= r.Y2; y += 1)
@@ -66,7 +71,7 @@ public class GraphBasedGenerator : IMapGenerator
         }
     }
 
-    private void CarveCorridor(in TileFlags[,] map, (int x, int y) a, (int x, int y) b)
+    private void CarveCorridor(in IntermediateMap map, (int x, int y) a, (int x, int y) b)
     {
         if (this.rng.Next(0, 2) == 0)
         {
@@ -80,24 +85,61 @@ public class GraphBasedGenerator : IMapGenerator
         }
     }
 
-    private static void CarveHorizontal(in TileFlags[,] map, int x1, int x2, int y)
+    private static void CarveHorizontal(in IntermediateMap map, int x1, int x2, int y)
     {
         int min = Math.Min(x1, x2);
         int max = Math.Max(x1, x2);
+
         for (int x = min; x <= max; x += 1)
         {
-            map[x, y] = TileFlags.Walkable | TileFlags.Path;
+            for (int dy = -1; dy <= 0; dy += 1)
+            {
+                int ny = y + dy;
+                if (ny >= 0 && ny < map.Height)
+                {
+                    map[x, ny] = TileFlags.Walkable | TileFlags.Path;
+                }
+            }
         }
     }
 
-    private static void CarveVertical(in TileFlags[,] map, int y1, int y2, int x)
+
+    private static void CarveVertical(in IntermediateMap map, int y1, int y2, int x)
     {
         int min = Math.Min(y1, y2);
         int max = Math.Max(y1, y2);
+
         for (int y = min; y <= max; y += 1)
         {
-            map[x, y] = TileFlags.Walkable | TileFlags.Path;
+            for (int dx = -1; dx <= 0; dx += 1)
+            {
+                int nx = x + dx;
+                if (nx >= 0 && nx < map.Width)
+                {
+                    map[nx, y] = TileFlags.Walkable | TileFlags.Path;
+                }
+            }
         }
+    }
+
+    private static bool EraseIllegalWalls(in IntermediateMap map)
+    {
+        bool changed = false;
+
+        for (int y = 0; y < map.Height; y += 1)
+        for (int x = 0; x < map.Width; x += 1)
+        {
+            if (map[x, y].IsWalkable()) { continue; }
+
+            if ((!map.IsWallLeftOf(x, y) && !map.IsWallRightOf(x, y)) ||
+                (!map.IsWallTopCenterOf(x, y) && !map.IsWallBottomCenterOf(x, y)))
+            {
+                changed = true;
+                map[x, y] = TileFlags.Walkable;
+            }
+        }
+
+        return changed;
     }
 
     private class Rect(int x, int y, int w, int h)
@@ -112,6 +154,16 @@ public class GraphBasedGenerator : IMapGenerator
         public bool Intersects(Rect other)
         {
             return this.X1 <= other.X2 && this.X2 >= other.X1 && this.Y1 <= other.Y2 && this.Y2 >= other.Y1;
+        }
+
+        public Rect Inflate(int amount)
+        {
+            return new Rect(
+                this.X1 - amount,
+                this.Y1 - amount,
+                this.X2 - this.X1 + (amount * 2),
+                this.Y2 - this.Y1 + (amount * 2)
+            );
         }
     }
 }
