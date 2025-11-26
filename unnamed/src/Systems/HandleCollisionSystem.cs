43c18c4f437e4fd55d37a6aa8939847bc2bc7d04
navlis_lnv <147550631+navlisData@users.noreply.github.com>
2025-11-26 11:12:15 +0100

    feat: add basic system that handles collided entities

---
+++ unnamed/src/Systems/HandleCollisionSystem.cs
@@ -0,0 +1,23 @@
+using Engine.Ecs;
+using Engine.Ecs.Systems;
+
+using unnamed.Components.Map;
+using unnamed.Components.Tags;
+
+namespace unnamed.systems;
+
+public class HandleCollisionSystem(World world) : EntitySetSystem<float>(world,
+    world.Query()
+        .With<Collided>()
+        .Build()
+)
+{
+
+    protected override void Update(float dt, in Entity e)
+    {
+        if (e.Has<Enemy>())
+        {
+            e.Add(new MarkedToDestroy());
+        }
+    }
+}
