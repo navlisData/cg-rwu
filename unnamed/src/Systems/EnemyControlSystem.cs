1bd7f65bd5a586efe74f0d4002dda6e42fcd45b7
navlisData <navlis.stuff@gmail.com>
2025-11-27 17:14:38 +0100

    Merge pull request #75
    * feat: add move animation to enemies in players range
    
    * chore: update enemy state in EnemyControlSystem

--- unnamed/src/Systems/EnemyControlSystem.cs
+++ unnamed/src/Systems/EnemyControlSystem.cs
@@ -4,6 +4,7 @@ using Engine.Ecs;
 using Engine.Ecs.Systems;
 
 using unnamed.Components.General;
+using unnamed.Components.Physics;
 using unnamed.Components.Rendering;
 using unnamed.Components.Tags;
 using unnamed.Enums;
@@ -24,10 +25,14 @@ public sealed class EnemyControlSystem(World world)
         ref NonDirectionalCharacter nonDirectionalCharacter = ref e.Get<NonDirectionalCharacter>();
         ref EnemyActionState enemyState = ref e.Get<EnemyActionState>();
 
+        EnemyAction action = e.Has<Velocity>()
+            ? EnemyAction.Move
+            : EnemyAction.Idle;
+
         EnemyAction currentState = args.actionHandler.TryUpdateAction(
             ref enemyState.CurrentAction,
             ref enemyState.RemainingTime,
-            desiredAction: EnemyAction.Idle,
+            desiredAction: action,
             out bool _
         );
 
